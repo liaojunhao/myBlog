a42f1721d0765d6c6639173ece35877beabce18d
liaojunhao <qq460200123@outlook.com>
2019-11-20 15:31:40 +0800

    更新远程

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,98 @@
+module.exports = {
+  "title": "junhao",
+  "description": "著名CV工程师一枚，千行代码，Bug何处藏",
+  // "dest": "public",
+  "base": "/myBlog/",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "sidebar": 'auto',
+    "themePicker":false,
+    "nav": [
+      {
+        "text": "首页",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "时间轴",
+        "link": "/timeLine/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "外链",
+        "icon": "reco-message",
+        "items": [
+          // {
+          //   "text": "NPM",
+          //   "link": "https://www.npmjs.com/~reco_luan",
+          //   "icon": "reco-npm"
+          // },
+          {
+            "text": "GitHub",
+            "link": "https://github.com/liaojunhao",
+            "icon": "reco-github"
+          }
+          // {
+          //   "text": "简书",
+          //   "link": "https://www.jianshu.com/u/cd674a19515e",
+          //   "icon": "reco-jianshu"
+          // },
+          // {
+          //   "text": "CSDN",
+          //   "link": "https://blog.csdn.net/recoluan",
+          //   "icon": "reco-csdn"
+          // },
+          // {
+          //   "text": "博客圆",
+          //   "link": "https://www.cnblogs.com/luanhewei/",
+          //   "icon": "reco-bokeyuan"
+          // },
+          // {
+          //   "text": "WeChat",
+          //   "link": "https://mp.weixin.qq.com/s/mXFqeUTegdvPliXknAAG_A",
+          //   "icon": "reco-wechat"
+          // }
+        ]
+      }
+    ],
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "分类"
+      },
+      "tag": {
+        "location": 3,
+        "text": "标签"
+      }
+    },
+    "logo": "/avatar.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "sidebar": "auto",
+    "lastUpdated": "Last Updated",
+    "author": "junhao",
+    // "record": "xxxx",
+    "startYear": "2017",
+    "huawei": false
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
