907b06ba145ce91929d45cfaa81a7afe63a0b764
liaojunhao <460200123@qq.com>
2020-02-17 17:47:19 +0800

    数据上报规范文档

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:liaojunhao/myBlog.git master:gh-pages
+git push -f https://github.com/liaojunhao/myBlog.git master:gh-pages
 
 cd -
